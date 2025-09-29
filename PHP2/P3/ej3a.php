682dfa6d877ca8635b092f803bf2b7fead89cca0
HughPF <126900977+HughPF@users.noreply.github.com>
2025-09-29 09:26:14 +0200

    Add files via upload

---
+++ PHP2/P3/ej3a.php
@@ -0,0 +1,27 @@
+<?php
+
+$bin= array();
+$alm=0;
+for ($j=0; $j < 20; $j++) { 
+    $bin[$j]=decbin($j);
+}
+
+echo "<table border='1' cellspacing='0' cellpadding='5'>";
+
+echo "<tr>";
+    echo "<th> Indice </th>";
+    echo "<th> Binario </th>";
+    echo "<th> Octal </th>";
+echo "</tr>";
+$suma=0;
+for ($i=0; $i < 20; $i++) { 
+    
+    echo "<tr>";
+    echo "<td>".$i."</td>";
+    echo "<td>".$bin[$i]."</td>";
+    echo "<td>".decoct(bindec($bin[$i]))."</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+?>
